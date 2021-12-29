2dad39fcaced637e24e1bcf61d6471a5a59ce567
JorisPaarde <jorispaarde@gmail.com>
2021-12-29 10:42:26 +0000

    Fixed navbar menu

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const navLinks = document.querySelectorAll('.nav-item');
+const menuToggle = document.getElementById('navbarNav');
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle: false});
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+});
+
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbarNav'
+  });
