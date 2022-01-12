8e8a9c96a2cd6f805754f2ffe10f46fcdc2088a1
JorisPaarde <jorispaarde@gmail.com>
2022-01-12 22:33:48 +0000

    improved site speed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
-const navLinks = document.querySelectorAll('.nav-item');
-const menuToggle = document.getElementById('navbarNav');
-const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle: false});
+const navLinks = document.querySelectorAll(".nav-item");
+const menuToggle = document.getElementById("navbarNav");
+const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
 
 navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
+  l.addEventListener("click", () => {
+    bsCollapse.toggle();
+  });
 });
 
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: '#navbarNav',
-    offset: 200
-  });
+  target: "#navbarNav",
+  offset: 200,
+});
+
+function init() {
+  var vidDefer = document.getElementsByTagName("iframe");
+  for (var i = 0; i < vidDefer.length; i++) {
+    if (vidDefer[i].getAttribute("data-src")) {
+      vidDefer[i].setAttribute("src", vidDefer[i].getAttribute("data-src"));
+    }
+  }
+}
+window.onload = init;
